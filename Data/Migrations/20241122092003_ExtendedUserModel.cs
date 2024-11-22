8e0cdc878d2e0d32ff89f84168903ff7b14fed51
KamikazeGoBoom <102337806+KamikazeGoBoom@users.noreply.github.com>
2024-11-22 17:24:39 +0800

    setup 2

---
+++ Data/Migrations/20241122092003_ExtendedUserModel.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ChatterBox.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ExtendedUserModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Birthday",
+                table: "AspNetUsers",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "DarkModeEnabled",
+                table: "AspNetUsers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeactivated",
+                table: "AspNetUsers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastSeen",
+                table: "AspNetUsers",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Birthday",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "DarkModeEnabled",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeactivated",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastSeen",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "AspNetUsers");
+        }
+    }
+}
--- obj/Debug/net8.0/ChatterBox.AssemblyInfo.cs
+++ obj/Debug/net8.0/ChatterBox.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatterBox")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf5698da91b9ed8d110b1389ff96768ed67b7497")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c52779b1dfa02d90843ee3bdcad5df1401c76a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatterBox")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatterBox")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
