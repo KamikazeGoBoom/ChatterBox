32ce9118958be80b74d5ecd60e2b3ecbadaa49e5
KamikazeGoBoom <102337806+KamikazeGoBoom@users.noreply.github.com>
2024-12-01 00:23:37 +0800

    theme

--- obj/Debug/net8.0/ChatterBox.AssemblyInfo.cs
+++ obj/Debug/net8.0/ChatterBox.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatterBox")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5086ef1a13f67dc3f9c69dd2ff43bc2e602654c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28ea71dc3ccc1772d8eb8995fcd820b58f7dcfb4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatterBox")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatterBox")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
